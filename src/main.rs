fe7ec69464be185fd90563514d0a5131b6ca4033
YofaGh <ghadiriyosef123@gmail.com>
2026-04-26 00:03:40 +0330

    Add Ctrl c signal

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,12 @@ async fn main() -> Result<(), errors::Error> {
     api::init_api_server(cancel_token.clone())?;
     webhook::init_webhook_dispatcher(cancel_token.clone());
     let telegram_bot_handle = telegram_bot::init_bot(cancel_token.clone());
-    while !cancel_token.is_cancelled() {
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    tokio::select! {
+        _ = cancel_token.cancelled() => {}
+        _ = tokio::signal::ctrl_c() => {
+            tracing::info!("Received Ctrl+C");
+            cancel_token.cancel();
+        }
     }
     tracing::warn!("Shutting down...");
     if let Some(handle) = telegram_bot_handle {
